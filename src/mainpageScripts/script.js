4305712c10af2fa266e1486772ad9e22d50183b4
Kacper <kacperkrok9@gmail.com>
2023-01-04 13:05:35 +0100

    Register form modal handling, changed functions to handle modals, added some css

--- src/mainpageScripts/script.js
+++ src/mainpageScripts/script.js
@@ -1,17 +1,23 @@
 let loginBtn = document.querySelector('.nav__login');
+let registerBtn = document.querySelector('.nav__register');
 let modalLogin = document.querySelector('.modal-login');
+let modalRegister = document.querySelector('.modal-register');
 let overlay = document.querySelector('.overlay');
-let closeModalBtn = document.querySelector('.close-modal--btn');
+let closeLoginModalBtn = document.querySelector('.close-loginModal--btn');
+let closeRegisterModalBtn = document.querySelector('.close-registerModal--btn');
 
-const openModal = () =>{
-    modalLogin.classList.remove('hidden');
+const openModal = (modal) =>{
+    modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 }
 
-const closeModal = () =>{
-    modalLogin.classList.add('hidden');
+const closeModal = (modal) =>{
+    modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
 
-loginBtn.addEventListener('click',openModal);
-closeModalBtn.addEventListener('click',closeModal);
+loginBtn.addEventListener('click',()=>{openModal(modalLogin)});
+closeLoginModalBtn.addEventListener('click',()=>{closeModal(modalLogin)});
+
+registerBtn.addEventListener('click',()=>{openModal(modalRegister)});
+closeRegisterModalBtn.addEventListener('click',()=>{closeModal(modalRegister)});
