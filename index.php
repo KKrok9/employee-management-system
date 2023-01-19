8b824359a8a6e61aa595aa5604db3f3aa29fc0b9
Kacper <kacperkrok9@gmail.com>
2023-01-19 14:14:18 +0100

    small css changes, added php files

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
 <?php
-class A {
-    public function foo() {
-    return 'class A, method foo()';
-    }
-   }
-   class B extends A{
-    public function foo() {
-    return 'class B, method foo() and parent: ' .
-    parent::foo();
-    }
-   }
-   $object = new B;
-   echo $object->foo();
+
 
 ?>
